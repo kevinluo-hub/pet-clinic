49a51a970d365e5d46901b39136faec34347956c
Kevin <lowing839613283@gmail.com>
2020-08-07 16:14:53 -0400

    added vet controller and Index html

---
+++ pet-clinic-web/src/main/java/kevinlearnspring_petClinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package kevinlearnspring_petClinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
