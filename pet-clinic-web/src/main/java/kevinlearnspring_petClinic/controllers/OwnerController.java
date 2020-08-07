3d5cd508418d56f5aedea08b48d645db6234b229
Kevin <lowing839613283@gmail.com>
2020-08-07 17:50:12 -0400

    added owner index and controller

---
+++ pet-clinic-web/src/main/java/kevinlearnspring_petClinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package kevinlearnspring_petClinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ListOwners() {
+        return "owners/index";
+    }
+}
