8492165488d5778c9053edfc5f5161f5324ce5fc
Kevin <lowing839613283@gmail.com>
2020-08-07 14:51:38 -0400

    Generic service interface created.

--- pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/OwnerService.java
@@ -1,15 +1,6 @@
 package kevinlearnspring_petClinic.services;
 import kevinlearnspring_petClinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long Id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 }
--- pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/PetService.java
+++ pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/PetService.java
@@ -2,12 +2,4 @@ package kevinlearnspring_petClinic.services;
 
 import kevinlearnspring_petClinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long Id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
