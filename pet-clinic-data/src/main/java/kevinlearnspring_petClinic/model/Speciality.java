ff3e6f9023d0ee2667e4706b9e433fa86f611bab
Kevin <lowing839613283@gmail.com>
2020-08-10 20:25:08 -0400

    added Speciality entity.

---
+++ pet-clinic-data/src/main/java/kevinlearnspring_petClinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package kevinlearnspring_petClinic.model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
