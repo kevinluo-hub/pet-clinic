a97cec5dac0959dcc2e83707a25ed79ec3a50276
Kevin <lowing839613283@gmail.com>
2020-08-13 13:17:53 -0400

    created speciality map services.

---
+++ pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package kevinlearnspring_petClinic.services;
+
+import kevinlearnspring_petClinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/kevinlearnspring_petClinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package kevinlearnspring_petClinic.services.map;
+
+import kevinlearnspring_petClinic.model.Speciality;
+import kevinlearnspring_petClinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
